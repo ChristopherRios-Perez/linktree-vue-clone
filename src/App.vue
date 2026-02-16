1b87f34ebf630abaeaa92a5a811c0035225f4bb8
Christopher Rios Perez <christopherr308@gmail.com>
2026-02-15 23:13:37 -0600

    Chapped App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="flex min-h-screen flex-col">
-    <NavMenu />
+  <div
+    class="bg-linear-br min-h-screen bg-linear-to-br from-gray-900 via-gray-800 to-gray-900 font-sans"
+  >
     <RouterView />
   </div>
 </template>
